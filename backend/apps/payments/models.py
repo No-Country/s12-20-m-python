84f4f4af531723e808e1a4bc79f4fe188e7ee307
Santiago Aquino <santiagoaquino01@hotmail.com>
2023-11-30 10:56:45 -0300

    adding payment models

--- backend/apps/payments/models.py
+++ backend/apps/payments/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+# from users.models import Adoption
 
-# Create your models here.
+
+class Pay(models.Model):
+    first_name = models.CharField(max_length=50, null=False)
+    last_name = models.CharField(max_length=50, null=False)
+    number_card = models.IntegerField(null=False)
+    cod_card = models.SmallIntegerField(null=False)
+    number_trees = models.IntegerField(null=False)
+    cost = models.IntegerField(null=False)
+    # adoption_id = models.ForeignKey(Adoption, on_delete=models.CASCADE)
+
+
+class Voucher(models.Model):
+    pass
+    # pay_id = models.ForeignKey(Pay, on_delete=models.CASCADE)
+    # adoption_id = models.ForeignKey(Adoption, on_delete=models.CASCADE)
