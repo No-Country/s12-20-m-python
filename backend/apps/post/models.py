ae2b403fe02513b476b4340ec03239278fd32fda
jorgea-hn <jorgeahenriqueznovoa@gmail.com>
2023-12-04 10:12:31 -0500

    Post model corregido

--- backend/apps/post/models.py
+++ backend/apps/post/models.py
@@ -1,22 +1,40 @@
 from django.db import models
+from model_utils.models import TimeStampedModel
 
 # Create your models here.
 
-class Post(models.Model):
+class Post(TimeStampedModel):
     id = models.AutoField(primary_key=True)
-    users_id = models.IntegerField()
+    # users_id = models.ForeignKey(User,on_delete=models.CASCADE )
 
-class ImgPost(models.Model):
+    class Meta:
+        verbose_name = 'Post'
+        verbose_name_plural = 'Posts'
+
+
+class ImgPost(TimeStampedModel):
     img_post_id = models.AutoField(primary_key=True)
+    img = models.ImageField(upload_to='images/')
     post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='img_posts')
-    img = models.ImageField(upload_to='img_posts')
 
-class VideoPost(models.Model):
+    class Meta:
+        verbose_name = 'ImgPost'
+        verbose_name_plural = 'ImgPosts'
+
+class VideoPost(TimeStampedModel):
     video_post_id = models.AutoField(primary_key=True)
+    video = models.FileField(upload_to='videos/')
     post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='video_posts')
-    video = models.FileField(upload_to='video_posts')
 
-class MessagePost(models.Model):
+    class Meta:
+        verbose_name = 'VideoPost'
+        verbose_name_plural = 'VideoPosts'
+
+class MessagePost(TimeStampedModel):
     message_post_id = models.AutoField(primary_key=True)
-    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='message_posts')
     message = models.TextField()
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='message_posts')
+
+    class Meta:
+        verbose_name = 'MessagePost'
+        verbose_name_plural = 'MessagePosts'
