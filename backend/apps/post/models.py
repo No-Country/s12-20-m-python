282751199a402b9d0ca81acb9325f8c06f9b9078
jorgea-hn <jorgeahenriqueznovoa@gmail.com>
2023-12-04 09:55:59 -0500

    Post model

--- backend/apps/post/models.py
+++ backend/apps/post/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
 # Create your models here.
+
+class Post(models.Model):
+    id = models.AutoField(primary_key=True)
+    users_id = models.IntegerField()
+
+class ImgPost(models.Model):
+    img_post_id = models.AutoField(primary_key=True)
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='img_posts')
+    img = models.ImageField(upload_to='img_posts')
+
+class VideoPost(models.Model):
+    video_post_id = models.AutoField(primary_key=True)
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='video_posts')
+    video = models.FileField(upload_to='video_posts')
+
+class MessagePost(models.Model):
+    message_post_id = models.AutoField(primary_key=True)
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='message_posts')
+    message = models.TextField()
