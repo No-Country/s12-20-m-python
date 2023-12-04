65337c409e08f31306c721c76008d2d94ec9fd04
scarolayton <scarolayton@gmail.com>
2023-12-04 10:00:39 -0500

    achivement tables

--- backend/apps/rewards/models.py
+++ backend/apps/rewards/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class Achievement(models.Model):
+  Description = models.TextField()
+  AchievementAssignment_id = models.ForeignKey('AchievementAssignment', on_delete=models.CASCADE)
+
+class AchievementAssignment(models.model):
+  Achievement_id = models.ForeignKey(Achievement, on_delete=models.CASCADE)
+  # User_id = models.ForeignKey(User, on_delete=models.CASCADE)
