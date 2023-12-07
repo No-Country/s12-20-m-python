14aa420e68ff10caef311362a5076a44b204b7c9
Santiago Aquino <santiagoaquino01@hotmail.com>
2023-12-07 17:20:23 -0300

    primer deploy v2

--- backend/config/settings/prod.py
+++ backend/config/settings/prod.py
@@ -1,4 +1,5 @@
 from .base import *
+import os
 
 DEBUG = True
 ALLOWED_HOSTS = ["*"]
@@ -7,12 +8,24 @@ ALLOWED_HOSTS = ["*"]
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': get_secret('DB_PROD_NAME'),
+        'USER': get_secret('DB_PROD_USER'),
+        'PASSWORD': get_secret('DB_PROD_PASSWORD'),
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/4.2/howto/static-files/
-STATIC_URL = "static/"
+STATIC_URL = '/static/'
+STATIC_DIR = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:5173",
+    'http://127.0.0.1:5173',
+]
