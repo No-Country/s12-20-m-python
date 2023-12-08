9d1f00a1ea0b09ef1cd97c7e086e9df896b2ea6d
Santiago Aquino <santiagoaquino01@hotmail.com>
2023-12-08 17:28:32 -0300

    deploy v11

--- backend/build.sh
+++ backend/build.sh
@@ -2,13 +2,8 @@
 # exit on error
 set -o errexit
 
-cd backend/
 pip install --upgrade pippip install --force-reinstall -U setuptools
 pip install -r requirements.txt
 
 python manage.py collectstatic --no-input
 python manage.py migrate
-
-cd ..
-mv backend/config/wsgi.py backend/
-ls
