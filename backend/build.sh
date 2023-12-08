0f1e01f1293f745535c7efca11e9796a13c6cfc0
Santiago Aquino <santiagoaquino01@hotmail.com>
2023-12-08 16:11:22 -0300

    deploy render v7

--- backend/build.sh
+++ backend/build.sh
@@ -5,8 +5,9 @@ set -o errexit
 cd backend/
 pip install --upgrade pippip install --force-reinstall -U setuptools
 pip install -r requirements.txt
-cd config/
-pwd
 
 python manage.py collectstatic --no-input
 python manage.py migrate
+
+cd config/
+pwd
