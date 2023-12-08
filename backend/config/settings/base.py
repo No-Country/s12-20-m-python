87271eb67d48e6b4770a0c294d7413a098ccbd6e
Santiago Aquino <santiagoaquino01@hotmail.com>
2023-12-08 15:02:29 -0300

    deploy render v3

--- backend/config/settings/base.py
+++ backend/config/settings/base.py
@@ -13,19 +13,19 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 from pathlib import Path
 import os
 
-from django.core.exceptions import ImproperlyConfigured
-import json
+# from django.core.exceptions import ImproperlyConfigured
+# import json
 
-with open("secret.json") as f:
-    secret = json.loads(f.read())
+# with open("secret.json") as f:
+#     secret = json.loads(f.read())
 
 
-def get_secret(secret_name, secrets=secret):
-    try:
-        return secrets[secret_name]
-    except:
-        msg = "la variable %s no existe" % secret_name
-    raise ImproperlyConfigured(msg)
+# def get_secret(secret_name, secrets=secret):
+#     try:
+#         return secrets[secret_name]
+#     except:
+#         msg = "la variable %s no existe" % secret_name
+#     raise ImproperlyConfigured(msg)
 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
