0989ab08b480def299f015159074fd124148cc78
Santiago Aquino <santiagoaquino01@hotmail.com>
2023-12-09 10:16:25 -0300

    added commands in build

--- backend/build.sh
+++ backend/build.sh
@@ -6,4 +6,9 @@ pip install --upgrade pippip install --force-reinstall -U setuptools
 pip install -r requirements.txt
 
 python manage.py collectstatic --no-input
+python manage.py makemigrations land
+python manage.py makemigrations payments
+python manage.py makemigrations post
+python manage.py makemigrations rewards
+python manage.py makemigrations users
 python manage.py migrate
