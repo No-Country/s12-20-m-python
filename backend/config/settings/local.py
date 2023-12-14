5a7ee773b22e8432d1e5ff0829c542ecd191f026
Santiago Aquino <santiagoaquino01@hotmail.com>
2023-12-14 20:22:21 -0300

    deploy

--- backend/config/settings/local.py
+++ backend/config/settings/local.py
@@ -23,6 +23,3 @@ DATABASES = {
 STATIC_URL = '/static/'
 STATIC_DIR = os.path.join(BASE_DIR, 'static')
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-
-MEDIA_URL = '/staticfiles/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'staticfiles/media')
