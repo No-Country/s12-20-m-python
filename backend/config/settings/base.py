eaec9492d32efb2d7ab04ffa1412b6d63a8515ab
Santiago Aquino <santiagoaquino01@hotmail.com>
2023-12-14 15:39:36 -0300

    parse class

--- backend/config/settings/base.py
+++ backend/config/settings/base.py
@@ -130,5 +130,5 @@ USE_TZ = True
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 #
 
-MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/staticfiles/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'staticfiles/media')
