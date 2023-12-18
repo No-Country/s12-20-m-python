22c0101747edc733c5599cc3337ce6a18a8842ae
Santiago Aquino <santiagoaquino01@hotmail.com>
2023-12-18 10:43:11 -0300

    update prod.py

--- backend/config/settings/prod.py
+++ backend/config/settings/prod.py
@@ -28,5 +28,5 @@ if not DEBUG:
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:5173",
     'http://127.0.0.1:5173',
-    'https://guardianesdelbosque.vercel.app/',
+    'https://guardianesdelbosque.vercel.app',
 ]
